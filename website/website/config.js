cc46611fdfb7a343e44d91f2da4705b5830cb3bd
CuvéeTRTL Node <leos.stehlik@gmail.com>
2021-03-07 00:08:42 +0000

    A few updates to the front-end

---
+++ website/website/config.js
@@ -0,0 +1,27 @@
+var api = ''
+
+var api_blockexplorer = ''
+
+var poolHost = ''
+
+var email = 'support@poolhost.com'
+
+var api_blockexplorer = ''
+
+var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
+
+var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
+
+var blockchainExplorer = 'https:///{symbol}/block/{id}'
+
+var transactionExplorer = 'https:///{symbol}/transaction/{id}'
+
+var themeCss = 'themes/default-theme.css'
+
+var networkStat = {
+  'bcn': [
+    ['bcn.mypool.online', 'http://bcn.mypool.online:8084'],
+    ['democats.org', 'http://pool.democats.org:7603']
+  ]
+}
+
